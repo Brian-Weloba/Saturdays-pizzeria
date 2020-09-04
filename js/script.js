5caa3d23137cbb2b6a227a0fa3632e4a531cf9a8
Brian-Weloba <bweloba@gmail.com>
2020-09-04 08:23:01 +0300

    add menu div

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 1580) {
+            $("#cover4").css({
+                "background-attachment": "scroll",
+                "background-position": "center",
+                "background-repeat": "no-repeat",
+                "background-size": "cover",
+                "size": "1920px, 1080px",
+                "max-width": "100%"
+            });
+        } else {
+            $("#cover4").css("background-attachment", "fixed");
+        }
+    });
+});*/
