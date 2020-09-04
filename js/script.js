235146b6ebcb46091e07a7b9f6dd185f297f01c2
Brian-Weloba <bweloba@gmail.com>
2020-09-04 08:45:32 +0300

    add missing semicolons

--- js/script.js
+++ js/script.js
@@ -15,14 +15,14 @@
     });
 });*/
 
-window.onscroll = function() { myFunction() };
+window.onscroll = function() { myFunction(); };
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
     if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+        navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
     }
