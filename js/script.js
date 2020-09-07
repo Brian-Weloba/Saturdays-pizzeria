b36057fd41f190f00ec50244a66135e406ff7cbc
Brian-Weloba <bweloba@gmail.com>
2020-09-07 13:59:54 +0300

    clean up code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-/*$(document).ready(function() {
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 1580) {
-            $("#cover4").css({
-                "background-attachment": "scroll",
-                "background-position": "center",
-                "background-repeat": "no-repeat",
-                "background-size": "cover",
-                "size": "1920px, 1080px",
-                "max-width": "100%"
-            });
-        } else {
-            $("#cover4").css("background-attachment", "fixed");
-        }
-    });
-});*/
-
 window.onscroll = function() { myFunction(); };
 
 var navbar = document.getElementById("navbar");
