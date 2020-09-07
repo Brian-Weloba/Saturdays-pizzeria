8f8956bb3c1c795c430f4052511bc9acb2c876af
Brian-Weloba <bweloba@gmail.com>
2020-09-07 10:34:22 +0300

    add menu pizza

--- js/script.js
+++ js/script.js
@@ -18,11 +18,13 @@
 window.onscroll = function() { myFunction(); };
 
 var navbar = document.getElementById("navbar");
+var menu = document.getElementById("menu-items");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
+        $("#menu-items").css("margin-top", "100px");
     } else {
         navbar.classList.remove("sticky");
     }
@@ -36,13 +38,13 @@ function filterSelection(c) {
     if (c == "all") c = "";
     // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
     for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+        RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
     }
 }
 
 // Show filtered elements
-function w3AddClass(element, name) {
+function AddClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
@@ -54,7 +56,7 @@ function w3AddClass(element, name) {
 }
 
 // Hide elements that are not selected
-function w3RemoveClass(element, name) {
+function RemoveClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
